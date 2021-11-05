3d317a7e8c582e3656bcd6e80f5ec53146d1f9c1
Bob Mead <bmead92@gmail.com>
2021-11-05 15:15:13 -0400

    docs are hard to read sometimes -__-

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
     /*Store hash in your db*/
     console.log(hash);
 
-    bcrypt.compare(someOtherPlaintextPassword, hash, (err, res) => {
+    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
         /*res == true or false*/
         console.log(res);
       });
